84765a49a592a62e64fcfb5d9b5f7be02f7edca2
mercury0517 <aki19960517@gmail.com>
2023-08-11 13:44:56 +0900

    Your first feature

---
+++ tcaPractice/tcaPractice/CounterFeature.swift
@@ -0,0 +1,77 @@
+//
+//  CounterFeature.swift
+//  tcaPractice
+//
+//  Created by 伊原明宏 on 2023/08/11.
+//
+
+import ComposableArchitecture
+import SwiftUI
+
+struct CounterView: View {
+    let store: StoreOf<CounterFeature>
+
+    var body: some View {
+        WithViewStore(self.store, observe: { $0 }) { viewStore in
+            VStack {
+                Text("\(viewStore.count)")
+                    .font(.largeTitle)
+                    .padding()
+                    .background(Color.black.opacity(0.1))
+                    .cornerRadius(10)
+                
+                HStack {
+                    Button("-") {
+                        viewStore.send(.decrementButtonTapped)
+                    }
+                    .font(.largeTitle)
+                    .padding()
+                    .background(Color.black.opacity(0.1))
+                    .cornerRadius(10)
+                    
+                    Button("+") {
+                        viewStore.send(.incrementButtonTapped)
+                    }
+                    .font(.largeTitle)
+                    .padding()
+                    .background(Color.black.opacity(0.1))
+                    .cornerRadius(10)
+                }
+            }
+        }
+    }
+}
+
+struct CounterPreview: PreviewProvider {
+    static var previews: some View {
+        CounterView(
+            store: Store(initialState: CounterFeature.State()) {
+                CounterFeature()
+            }
+        )
+    }
+}
+
+struct CounterFeature: Reducer {
+    struct State {
+        var count = 0
+    }
+
+    enum Action {
+        case decrementButtonTapped
+        case incrementButtonTapped
+    }
+
+    func reduce(into state: inout State, action: Action) -> Effect<Action> {
+        switch action {
+        case .decrementButtonTapped:
+            state.count -= 1
+            return .none
+        case .incrementButtonTapped:
+            state.count += 1
+            return .none
+        }
+    }
+}
+
+extension CounterFeature.State: Equatable {}
--- tcaPractice/tcaPractice/tcaPracticeApp.swift
+++ tcaPractice/tcaPractice/tcaPracticeApp.swift
@@ -5,13 +5,19 @@
 //  Created by 伊原明宏 on 2023/08/11.
 //
 
+import ComposableArchitecture
 import SwiftUI
 
 @main
 struct tcaPracticeApp: App {
+    static let store = Store(initialState: CounterFeature.State()) {
+        CounterFeature()
+            ._printChanges()
+    }
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            CounterView(store: tcaPracticeApp.store)
         }
     }
 }
